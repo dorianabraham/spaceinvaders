ebb9cad7276a35a08f88fc0876d9522bd3ecc977
Dorian Abraham <dorian.abraham@etu.unilim.fr>
2019-05-14 14:40:28 +0200

    positionner un vaisseau avec une dimension

--- src/test/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
+++ src/test/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
@@ -4,16 +4,45 @@ public class Vaisseau {
 
 	int x;
 	int y;
+	int longueur;
+	int hauteur;
 
-	public Vaisseau(int x, int y) {
-		this.x = x;
-		this.y = y;
+	public Vaisseau(int longueur, int hauteur) {
+		this(longueur, hauteur, 0, 0);
 	}
 	
-	public boolean occupeLaPosition(int x, int y) {
-		return (this.x==x) && (this.y==y);
+    public Vaisseau(int longueur, int hauteur, int x, int y) {
+	   this.longueur=longueur;
+	   this.hauteur=hauteur;
+	   this.x = x;
+	   this.y = y;
+    }
+    
+    public boolean occupeLaPosition(int x, int y) {
+	     return (estAbscisseCouverte(x) && estOrdonneeCouverte(y));
+    }
+
+	public boolean estOrdonneeCouverte(int y) {
+		return (ordonneeLaPlusBasse()<=y) && (y<=ordonneeLaPlusHaute());
 	}
-	
+
+	public int ordonneeLaPlusHaute() {
+		return this.y;
+	}
+
+	public int ordonneeLaPlusBasse() {
+		return ordonneeLaPlusHaute()-this.hauteur+1;
+	}
+
+	public boolean estAbscisseCouverte(int x) {
+		return (abscisseLaPlusAGauche()<=x) && (x<=abscisseLaPlusADroite());
+	}
+
+	public int abscisseLaPlusADroite() {
+		return this.x+this.longueur-1;
+	}
+
+
     public void seDeplacerVersLaDroite() {
 	      this.x = this.x + 1 ;
     }
@@ -22,8 +51,13 @@ public class Vaisseau {
 	      this.x = this.x - 1 ;
   }
     
-   	public int abscisse() {
+   	public int abscisseLaPlusAGauche() {
         return this.x;
 	}
+
+    public void positionner(int x, int y) {
+	    this.x = x;
+	    this.y = y;
+    }
 
 }
--- src/test/java/fr/unilim/iut/spaceinvaders/utils/HorsEspaceJeuException.java
+++ src/test/java/fr/unilim/iut/spaceinvaders/utils/HorsEspaceJeuException.java
@@ -7,5 +7,8 @@ class HorsEspaceJeuException extends RuntimeException {
     public HorsEspaceJeuException(String message) {
 	    super(message);
     }
+    
+
 
 }
+
